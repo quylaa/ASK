a33bf7d6fded81b987215c4b573c926e391f079b
Anna <gracelessmaiden@gmail.com>
2018-03-29 14:59:48 -0600

    port port

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride())
 app.use(cookieParser())
-app.set('port', process.env.APIPORT || 8001)
+app.set('port', process.env.APIPORT || 8000)
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
